4b556f00044376389d0aefbd76de270c0a8e3213
Mayank Kaushik <mayankkaushik7264@gmail.com>
2026-05-22 07:21:44 +0530

    Port updated to 10000

--- backend/dev-api.ts
+++ backend/dev-api.ts
@@ -54,5 +54,5 @@ app.use("/api/contact", contactRoutes);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
